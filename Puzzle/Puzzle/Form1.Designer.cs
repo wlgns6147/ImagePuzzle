4c748b1549c62bef0056b2351fe0ed44d951a869
wlgns6147 <44218322+wlgns6147@users.noreply.github.com>
2018-10-17 11:30:42 +0900

    Upload ImagePuzzle

---
+++ Puzzle/Puzzle/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pause_label = new System.Windows.Forms.Label();
+            this.exitbtn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.time_label = new System.Windows.Forms.Label();
+            this.tmrTimeElapse = new System.Windows.Forms.Timer(this.components);
+            this.pausebtn = new System.Windows.Forms.Button();
+            this.start4x4btn = new System.Windows.Forms.Button();
+            this.start3x3btn = new System.Windows.Forms.Button();
+            this.startbtn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.이미지선택ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.img_1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.img_2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.img_3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.img_4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.img_5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Location = new System.Drawing.Point(43, 76);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(273, 273);
+            this.panel1.TabIndex = 0;
+            // 
+            // pause_label
+            // 
+            this.pause_label.AutoSize = true;
+            this.pause_label.Font = new System.Drawing.Font("문체부 제목 돋음체", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.pause_label.Location = new System.Drawing.Point(293, 205);
+            this.pause_label.Name = "pause_label";
+            this.pause_label.Size = new System.Drawing.Size(128, 28);
+            this.pause_label.TabIndex = 9;
+            this.pause_label.Text = "일시정지";
+            this.pause_label.Visible = false;
+            // 
+            // exitbtn
+            // 
+            this.exitbtn.Location = new System.Drawing.Point(230, 390);
+            this.exitbtn.Name = "exitbtn";
+            this.exitbtn.Size = new System.Drawing.Size(90, 30);
+            this.exitbtn.TabIndex = 9;
+            this.exitbtn.Text = "종료";
+            this.exitbtn.UseVisualStyleBackColor = true;
+            this.exitbtn.Click += new System.EventHandler(this.exitbtn_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel2.Location = new System.Drawing.Point(403, 76);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(250, 250);
+            this.panel2.TabIndex = 9;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "퍼즐";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(403, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "원본";
+            // 
+            // time_label
+            // 
+            this.time_label.AutoSize = true;
+            this.time_label.Font = new System.Drawing.Font("Cooper Blk BT", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.time_label.Location = new System.Drawing.Point(412, 329);
+            this.time_label.Name = "time_label";
+            this.time_label.Size = new System.Drawing.Size(241, 58);
+            this.time_label.TabIndex = 9;
+            this.time_label.Text = "00:00:00";
+            // 
+            // tmrTimeElapse
+            // 
+            this.tmrTimeElapse.Enabled = true;
+            this.tmrTimeElapse.Interval = 1000;
+            this.tmrTimeElapse.Tick += new System.EventHandler(this.UpdateTimeElapsed);
+            // 
+            // pausebtn
+            // 
+            this.pausebtn.Location = new System.Drawing.Point(134, 390);
+            this.pausebtn.Name = "pausebtn";
+            this.pausebtn.Size = new System.Drawing.Size(90, 30);
+            this.pausebtn.TabIndex = 10;
+            this.pausebtn.Text = "일시정지";
+            this.pausebtn.UseVisualStyleBackColor = true;
+            this.pausebtn.Click += new System.EventHandler(this.pausebtn_Click);
+            // 
+            // start4x4btn
+            // 
+            this.start4x4btn.Location = new System.Drawing.Point(528, 390);
+            this.start4x4btn.Name = "start4x4btn";
+            this.start4x4btn.Size = new System.Drawing.Size(100, 30);
+            this.start4x4btn.TabIndex = 11;
+            this.start4x4btn.Text = "4 x 4 퍼즐";
+            this.start4x4btn.UseVisualStyleBackColor = true;
+            this.start4x4btn.Click += new System.EventHandler(this.start44btn_Click);
+            // 
+            // start3x3btn
+            // 
+            this.start3x3btn.Location = new System.Drawing.Point(411, 390);
+            this.start3x3btn.Name = "start3x3btn";
+            this.start3x3btn.Size = new System.Drawing.Size(100, 30);
+            this.start3x3btn.TabIndex = 12;
+            this.start3x3btn.Text = "3 x 3 퍼즐";
+            this.start3x3btn.UseVisualStyleBackColor = true;
+            this.start3x3btn.Click += new System.EventHandler(this.start33btn_Click);
+            // 
+            // startbtn
+            // 
+            this.startbtn.Location = new System.Drawing.Point(38, 390);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(90, 30);
+            this.startbtn.TabIndex = 13;
+            this.startbtn.Text = "시작";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.startbtn_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.이미지선택ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(684, 28);
+            this.menuStrip1.TabIndex = 15;
+            this.menuStrip1.Text = "menuStrip2";
+            // 
+            // 이미지선택ToolStripMenuItem
+            // 
+            this.이미지선택ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.img_1,
+            this.img_2,
+            this.img_3,
+            this.img_4,
+            this.img_5});
+            this.이미지선택ToolStripMenuItem.Name = "이미지선택ToolStripMenuItem";
+            this.이미지선택ToolStripMenuItem.Size = new System.Drawing.Size(131, 24);
+            this.이미지선택ToolStripMenuItem.Text = "이미지 변경하기";
+            // 
+            // img_1
+            // 
+            this.img_1.Name = "img_1";
+            this.img_1.Size = new System.Drawing.Size(92, 26);
+            this.img_1.Text = "1";
+            this.img_1.Click += new System.EventHandler(this.img_1_Click);
+            // 
+            // img_2
+            // 
+            this.img_2.Name = "img_2";
+            this.img_2.Size = new System.Drawing.Size(92, 26);
+            this.img_2.Text = "2";
+            this.img_2.Click += new System.EventHandler(this.img_2_Click);
+            // 
+            // img_3
+            // 
+            this.img_3.Name = "img_3";
+            this.img_3.Size = new System.Drawing.Size(92, 26);
+            this.img_3.Text = "3";
+            this.img_3.Click += new System.EventHandler(this.img_3_Click);
+            // 
+            // img_4
+            // 
+            this.img_4.Name = "img_4";
+            this.img_4.Size = new System.Drawing.Size(92, 26);
+            this.img_4.Text = "4";
+            this.img_4.Click += new System.EventHandler(this.img_4_Click);
+            // 
+            // img_5
+            // 
+            this.img_5.Name = "img_5";
+            this.img_5.Size = new System.Drawing.Size(92, 26);
+            this.img_5.Text = "5";
+            this.img_5.Click += new System.EventHandler(this.img_5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 452);
+            this.Controls.Add(this.startbtn);
+            this.Controls.Add(this.start3x3btn);
+            this.Controls.Add(this.start4x4btn);
+            this.Controls.Add(this.pause_label);
+            this.Controls.Add(this.pausebtn);
+            this.Controls.Add(this.time_label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.exitbtn);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "퍼즐 맞추기";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button exitbtn;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label time_label;
+        private System.Windows.Forms.Timer tmrTimeElapse;
+        private System.Windows.Forms.Button pausebtn;
+        private System.Windows.Forms.Label pause_label;
+        private System.Windows.Forms.Button start4x4btn;
+        private System.Windows.Forms.Button start3x3btn;
+        private System.Windows.Forms.Button startbtn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 이미지선택ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem img_1;
+        private System.Windows.Forms.ToolStripMenuItem img_2;
+        private System.Windows.Forms.ToolStripMenuItem img_3;
+        private System.Windows.Forms.ToolStripMenuItem img_4;
+        private System.Windows.Forms.ToolStripMenuItem img_5;
+    }
+}
+
---
+++ Puzzle/Puzzle/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Diagnostics;
+
+namespace Puzzle
+{
+    public partial class Form1 : Form
+    {
+        int[,] PuzzleTable;     // 퍼즐상태 확인용 퍼즐 테이블
+        Button[] btn;           // 퍼즐 버튼 배열 변수
+        Image[] piece_image;    // 조각 이미지 배열 변수
+
+        int ROW, COL, NUM = 0;  // 숨긴 퍼즐의 위치, 번호
+
+        int num_of_puzzle;      // 퍼즐 개수
+        int puzzle_row_col;     // 퍼즐 가로세로 개수
+        int btn_size;           // 퍼즐 버튼 크기 (가로세로)
+
+        Image img = new Bitmap(Properties.Resources._1);    // 사진 불러오기
+        Size resize = new Size(200, 200);
+        Stopwatch timer = new Stopwatch();
+        DialogResult YesOrNo = new DialogResult();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start44btn_Click(object sender, EventArgs e)
+        {
+            if (time_label.Text != "00:00:00")
+            {
+                YesOrNo = MessageBox.Show("현재 게임을 끝내시겠습니까 ?", "4 x 4 퍼즐", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+            if (YesOrNo == DialogResult.Yes || time_label.Text == "00:00:00")
+            {
+                foreach (Button btn in panel1.Controls)
+                {
+                    btn.Visible = false;
+                }
+                num_of_puzzle = 16;
+                puzzle_row_col = 4;
+                btn_size = 60;
+
+                createButton();
+
+                timer.Reset();
+                time_label.Text = "00:00:00";
+            }
+        }
+
+        private void start33btn_Click(object sender, EventArgs e)
+        {
+            if (time_label.Text != "00:00:00")
+            {
+                YesOrNo = MessageBox.Show("현재 게임을 끝내시겠습니까 ?", "3 x 3 퍼즐", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+            if (YesOrNo == DialogResult.Yes || time_label.Text == "00:00:00")
+            {
+                foreach (Button btn in panel1.Controls)
+                {
+                    btn.Visible = false;
+                }
+                num_of_puzzle = 9;
+                puzzle_row_col = 3;
+                btn_size = 80;
+
+                createButton();
+
+                timer.Reset();
+                time_label.Text = "00:00:00";
+            }
+        }
+
+        private void createButton()
+        {
+            Size resize = new Size(240, 240);           // 240 x 240 으로 사진크기 수정
+            Image re_image = new Bitmap(img, resize);
+            Graphics G = Graphics.FromImage(re_image);
+            
+            btn = new Button[num_of_puzzle];            // 버튼 생성 
+            piece_image = new Image[num_of_puzzle];     // 이미지 생성
+            PuzzleTable = new int[puzzle_row_col, puzzle_row_col]; // 가상테이블 생성
+
+            int Point_X;
+            int Point_Y;
+
+            for (int i = 0; i < btn.Length; i++)
+            {
+                // Point_X = 0, 1, 2, 0, 1, 2, 0, 1, 2 혹은
+                // Point_X = 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3
+                Point_X = (i % puzzle_row_col);
+                // Point_X = 0, 0, 0, 1, 1, 1, 2, 2, 2 혹은
+                // Point_X = 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2
+                Point_Y = (i / puzzle_row_col);
+                btn[i] = new Button();
+                btn[i].Name = i.ToString();
+                btn[i].Size = new Size(btn_size, btn_size);
+                btn[i].Location = new Point(btn_size * Point_X, btn_size * Point_Y);
+                btn[i].Font = new Font("굴림", 8);
+                btn[i].Click += new EventHandler(OnClick);
+                
+                Bitmap bmp = new Bitmap(btn_size, btn_size, G); // 버튼 크기의 이미지 틀 생성
+                Graphics g = Graphics.FromImage(bmp);
+
+                // 이미지 자르기
+                g.DrawImage(re_image, 0, 0, new Rectangle(Point_X * btn_size, Point_Y * btn_size, btn_size, btn_size), GraphicsUnit.Pixel);
+                panel1.Controls.Add(btn[i]);              // panel1에 버튼 배열 추가
+                this.piece_image[i] = btn[i].Image = bmp; // 조각 이미지 배열 = 버튼 배열.image = bmp
+            }
+        }
+
+        // 퍼즐 섞기
+        private void shuffle()
+        {
+            int[] ran_num_arr = new int[num_of_puzzle];  // 랜덤 생성한 번호들을 모아놓기 위한 배열
+            int ran_num;                                 // 랜덤 생성한 숫자를 담을 변수
+            Random rand = new Random();
+            for (int i = 0; i < num_of_puzzle; i++)
+            {
+                reRandom:                                // goto문 받는 곳
+                ran_num = rand.Next(num_of_puzzle) + 1;  // 난수 생성 ran_num = 1~9
+                foreach (int j in ran_num_arr)           // 기존에 생성된 번호인지 확인
+                {
+                    if (j == ran_num)                    // 이미 생성된 번호 일때
+                        goto reRandom;                   // reRandom 으로
+                }
+                ran_num_arr[i] = ran_num;                // 랜덤 생성한 번호들을 모아놓기 위한 배열
+                btn[i].Image = piece_image[ran_num - 1]; // 퍼즐 버튼에 랜덤 이미지 작성
+                btn[i].Text = ran_num.ToString();
+                if (ran_num == num_of_puzzle)            // 마지막 퍼즐은 보이지 않게 설정
+                    btn[i].Visible = false;
+            }
+            //2차원 배열(PuzzleTable)에 현재 대입된 값들을 할당한다. (숨김 버튼 위치 찾기)
+            int k = 0;
+            for (int row = 0; row < puzzle_row_col; row++)
+            {
+                for (int col = 0; col < puzzle_row_col; col++)
+                {
+                    PuzzleTable[row, col] = ran_num_arr[k]; // 2차원 배열에 현재 버튼
+                    if (ran_num_arr[k] == num_of_puzzle)    // 퍼즐상태 확인 테이블에 숨김 버튼 위치 저장
+                    {
+                        //숨겨진 버튼의 전역변수에 저장
+                        this.ROW = row;
+                        this.COL = col;
+                        this.NUM = k;
+                    }
+                    k++;
+                }
+            }
+        }
+
+        // 퍼즐 클릭 이벤트
+        public void OnClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;                        // 클릭한 버튼의 객체를 가져옴
+            movePuzzle(Convert.ToInt32(btn.Name), btn.Text);    // 버튼의 아이디와 텍스트를 보냄
+        }
+
+        // 퍼즐 이동
+        private void movePuzzle(int count, string btn_text)
+        {
+            // 현재 클릭한 버튼의 위치찾기 (2차원배열)
+            int row, col = 0;
+            for (row = 0; row < puzzle_row_col; row++)
+            {
+                for (col = 0; col < puzzle_row_col; col++)
+                {
+                    // 현재 할당된 행과 열을 찾앗을경우 Check_Move로
+                    if (PuzzleTable[row, col] == Convert.ToInt32(btn_text))
+                        goto Check_Move;
+                }
+            }
+            Check_Move:     // goto문 받는 곳
+            bool moving = false;
+            if ((ROW == row) && Math.Abs(COL - col) == 1)   // 행으로 움직일 수 있는 상태인지 확인
+                moving = true;      
+            if ((COL == col) && Math.Abs(ROW - row) == 1)   // 열로 움직을 수 있는 상태인지 확인
+                moving = true;
+            if (!moving)    // 움직이지 못하는 상태면 리턴
+                return;
+
+            //숨겨진 버튼에 선택한 번호 옮겨놓기
+            PuzzleTable[ROW, COL] = Convert.ToInt32(btn_text);  // 숨겨진 버튼에 대입
+            PuzzleTable[row, col] = num_of_puzzle;              // 클릭한 버튼은 빈 번호로
+
+            //숨겨진 버튼에 클릭한 버튼의 이미지 텍스트 보여주기
+            btn[NUM].Image = btn[count].Image;
+            btn[NUM].Visible = true;
+            btn[NUM].Text = btn_text;
+
+            //클릭한 버튼 다시 숨기기
+            btn[count].Text = num_of_puzzle.ToString();
+            btn[count].Visible = false;
+            btn[count].Image = piece_image[num_of_puzzle - 1];
+
+            //숨겨진 번호들 업데이트
+            this.ROW = row;
+            this.COL = col;
+            this.NUM = count;
+
+            // 퍼즐을 다 맞췄는지 확인
+            int compare = 0;
+            for (row = 0; row < puzzle_row_col; row++)
+            {
+                for (col = 0; col < puzzle_row_col; col++)
+                {
+                    compare++;
+                    if (PuzzleTable[row, col] != compare)
+                    {
+                        return;  // 하나라도 다를 경우 함수를 종료
+                    }
+
+                }
+            }
+            // 모두 통과한 경우
+            YesOrNo = MessageBox.Show("축하합니다", "퍼즐 완성", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            timer.Stop();
+        }
+
+        // panel2 에 이미지 넣기
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(img, 0, 0, 200, 200);
+        }
+
+        #region 시작, 일시정지, 종료 버튼
+        // 일시정지 버튼
+        private void pausebtn_Click(object sender, EventArgs e)
+        {
+            if (pausebtn.Text == "일시정지")
+            {
+                timer.Stop();
+                panel1.Visible = false;
+                panel2.Visible = false;
+                label1.Visible = false;
+                label2.Visible = false;
+                pause_label.Visible = true;
+                pausebtn.Text = "재시작";
+            }
+            else
+            {
+                timer.Start();
+                panel1.Visible = true;
+                panel2.Visible = true;
+                label1.Visible = false;
+                label2.Visible = false;
+                pause_label.Visible = false;
+                pausebtn.Text = "일시정지";
+            }
+        }
+
+        // 종료 버튼
+        private void exitbtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // 시작 버튼
+        private void startbtn_Click(object sender, EventArgs e)
+        {
+            shuffle();   // 이미지 섞기
+            if (time_label.Text == "00:00:00")
+                timer.Start();
+        }
+        #endregion
+
+        // 타이머
+        private void UpdateTimeElapsed(object sender, EventArgs e)
+        {
+            if (timer.Elapsed.ToString() != "00:00:00")
+                time_label.Text = timer.Elapsed.ToString().Remove(8);
+            if (timer.Elapsed.ToString() == "00:00:00")
+                pausebtn.Enabled = false;
+            else
+                pausebtn.Enabled = true;
+        }
+
+        #region menuStrip 이미지 선택 (1~5)
+        private void img_5_Click(object sender, EventArgs e)
+        {
+            img = new Bitmap(Properties.Resources._5);
+            panel2.BackgroundImage = new Bitmap(img, resize);
+        }
+
+        private void img_4_Click(object sender, EventArgs e)
+        {
+            img = new Bitmap(Properties.Resources._4);
+            panel2.BackgroundImage = new Bitmap(img, resize);
+        }
+
+        private void img_3_Click(object sender, EventArgs e)
+        {
+            img = new Bitmap(Properties.Resources._3);
+            panel2.BackgroundImage = new Bitmap(img, resize);
+        }
+
+        private void img_2_Click(object sender, EventArgs e)
+        {
+            img = new Bitmap(Properties.Resources._2);
+            panel2.BackgroundImage = new Bitmap(img, resize);
+        }
+
+        private void img_1_Click(object sender, EventArgs e)
+        {
+            img = new Bitmap(Properties.Resources._1);
+            panel2.BackgroundImage = new Bitmap(img, resize);
+        }
+        #endregion
+    }
+}
